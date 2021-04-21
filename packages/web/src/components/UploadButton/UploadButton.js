1f10d67d3f76796d04fbd84bf35d2c85fb8d14cc
Fabrizio Ervini <faber.rvn@gmail.com>
2021-04-21 15:26:24 +0200

    fix: upload button

--- packages/web/src/components/UploadButton/UploadButton.js
+++ packages/web/src/components/UploadButton/UploadButton.js
@@ -1,24 +1,24 @@
-import React from "react";
+import { faCloudUploadAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { bool, func } from "prop-types";
+import React from "react";
 
-const addSong = <FontAwesomeIcon icon={faPlus} />;
+const addSong = <FontAwesomeIcon icon={faCloudUploadAlt} />;
 
 function UploadButton({ showModal, setShowModal }) {
   function handleClick() {
     setShowModal(true);
   }
   return (
-    <>
-      <button
-        type="button"
-        className="focus:outline-none openModal"
-        onClick={handleClick}
-      >
-        {addSong}
-      </button>
-    </>
+    <button
+      type="button"
+      title="upload a song"
+      aria-label="upload a song"
+      className="focus:outline-none openModal mr-4"
+      onClick={handleClick}
+    >
+      {addSong}
+    </button>
   );
 }
 
