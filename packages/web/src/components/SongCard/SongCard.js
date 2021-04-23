35f51e80dad1d6e195d247104ba99372562f4314
Fabrizio Ervini <faber.rvn@gmail.com>
2021-04-23 16:18:34 +0200

    fix: fix merge import issue

--- packages/web/src/components/SongCard/SongCard.js
+++ packages/web/src/components/SongCard/SongCard.js
@@ -1,14 +1,13 @@
-import React, { useState } from "react";
-
 import {
   faHeart as farHeart,
   faPlayCircle as farPlayCircle,
 } from "@fortawesome/free-regular-svg-icons";
 import { faEllipsisH, faHeart } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { func, object } from "prop-types";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { playSong } from "../../redux/player/player-actions";
-import { object, func } from "prop-types";
 import SongDialogue from "../SongDialogue";
 import "./SongCard.scss";
 
@@ -24,7 +23,6 @@ function SongCard({
   const [isFavorite, setIsFavorite] = useState(false);
   const dispatch = useDispatch();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
 
   return (
     <div className="my-1 mb-6 px-1 w-full max-w-sm sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 lg:my-4 lg:px-4">
