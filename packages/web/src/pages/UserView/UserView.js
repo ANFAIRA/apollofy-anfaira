e163cc4d0c9b6a866ff27d0b0bddd2aedf95b6d1
iarrieta90 <irati_arrieta90@hotmail.com>
2021-05-14 09:48:04 +0200

    feat: add slider to profile and userView, and add scrollToTop function to main layout

--- packages/web/src/pages/UserView/UserView.js
+++ packages/web/src/pages/UserView/UserView.js
@@ -6,6 +6,7 @@ import SongCard from "../../components/SongCard";
 import PlaylistCard from "../../components/PlayListCard";
 import Main from "../../layout/Main";
 import UserProfileLayout from "../../layout/UserProfileLayout";
+import Slider from "../../components/Slider";
 
 const UserView = () => {
   const { playlistsByID } = useSelector((state) => state.playlists);
@@ -29,40 +30,58 @@ const UserView = () => {
         </h2>
         <hr className="border-gray-600 pb-2 " />
         <section className="flex flex-wrap justify-center sm:justify-start mx-1 lg:mx-4">
-          {uploadedSongs?.slice(0, 6).map((song) => (
-            <SongCard key={songsByID[song]._id} song={songsByID[song]} />
-          ))}
+          <Slider>
+            {uploadedSongs?.slice(0, 6).map((song) => (
+              <SongCard
+                key={songsByID[song]._id}
+                song={songsByID[song]}
+                slide
+              />
+            ))}
+          </Slider>
         </section>
         <h2 className="pb-2 font-semibold mt-10 capitalize">
           {username}&apos;s Playlists
         </h2>
         <hr className="border-gray-600 pb-2 " />
         <section className="flex flex-wrap justify-center sm:justify-start mx-1 lg:mx-4">
-          {uploadedPlaylist?.slice(0, 6).map((playlist) => (
-            <PlaylistCard
-              key={playlistsByID[playlist]?._id}
-              playlist={playlistsByID[playlist]}
-              location={`playlist/${playlistsByID[playlist]?._id}`}
-            />
-          ))}
+          <Slider>
+            {uploadedPlaylist?.slice(0, 6).map((playlist) => (
+              <PlaylistCard
+                key={playlistsByID[playlist]?._id}
+                playlist={playlistsByID[playlist]}
+                location={`playlist/${playlistsByID[playlist]?._id}`}
+                slide
+              />
+            ))}
+          </Slider>
         </section>
         <h2 className="pb-2 font-semibold mt-10 ">Favorites</h2>
         <hr className="border-gray-600 pb-2 " />
         <section className="flex flex-wrap justify-center sm:justify-start mx-1 lg:mx-4">
-          {likedSongs?.slice(0, 6).map((song) => (
-            <SongCard key={songsByID[song]._id} song={songsByID[song]} />
-          ))}
+          <Slider>
+            {likedSongs?.slice(0, 6).map((song) => (
+              <SongCard
+                key={songsByID[song]._id}
+                song={songsByID[song]}
+                slide
+              />
+            ))}
+          </Slider>
         </section>
         <h2 className="pb-2 font-semibold mt-10 ">Following</h2>
         <hr className="border-gray-600 pb-2 " />
         <section className="flex flex-wrap justify-center sm:justify-start mx-1 lg:mx-4">
-          {followedPlaylist?.slice(0, 6).map((playlist) => (
-            <PlaylistCard
-              key={playlistsByID[playlist]?._id}
-              playlist={playlistsByID[playlist]}
-              location={`playlist/${playlistsByID[playlist]?._id}`}
-            />
-          ))}
+          <Slider>
+            {followedPlaylist?.slice(0, 6).map((playlist) => (
+              <PlaylistCard
+                key={playlistsByID[playlist]?._id}
+                playlist={playlistsByID[playlist]}
+                location={`playlist/${playlistsByID[playlist]?._id}`}
+                slide
+              />
+            ))}
+          </Slider>
         </section>
       </UserProfileLayout>
     </Main>
