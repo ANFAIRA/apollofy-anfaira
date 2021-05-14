499c27f66b2a061201544ed13c8915e8f02c0872
iarrieta90 <irati_arrieta90@hotmail.com>
2021-05-14 07:24:18 +0200

    fix: take out conditional rendering of playlistCard component

--- packages/web/src/components/PlayListCard/PlayListCard.js
+++ packages/web/src/components/PlayListCard/PlayListCard.js
@@ -4,7 +4,7 @@ import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { fetchPlaylistById } from "../../redux/playlist/playlist-actions";
 
-function PlayListCard({ title, location, playlist = null, slide = false }) {
+function PlayListCard({ location, playlist, slide = false }) {
   const history = useHistory();
   const dispatch = useDispatch();
 
@@ -21,59 +21,37 @@ function PlayListCard({ title, location, playlist = null, slide = false }) {
           : "my-1 mb-6 px-1 w-full max-w-sm sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 lg:my-4 lg:px-4"
       }
     >
-      <div className={playlist ? "card" : ""}>
+      <div className="card">
         <button
           type="button"
-          className={
-            playlist
-              ? "relative z-10 flex flex-col justify-center h-full object-cover bg-center shadow-md overflow-hidden cursor-pointer focus:outline-none"
-              : "flex flex-col justify-between w-96 sm:w-96 h-96 bg-white bg-center shadow-md overflow-hidden cursor-pointer mt-8 playlist-card focus:outline-none"
-          }
+          className="relative z-10 flex flex-col justify-center h-full object-cover bg-center shadow-md overflow-hidden cursor-pointer focus:outline-none"
           onClick={handleClick}
         >
-          {playlist && (
-            <img src={playlist ? playlist.thumbnail : ""} alt="thumbnail" />
-          )}
-          <div
-            className={
-              playlist
-                ? "absolute z-20 bg-purple-800 bg-opacity-85 shadow-md rounded-r-xl px-2 py-2 flex flex-col w-5/6 h-2/6 mt-16"
-                : "absolute z-20 bg-purple-800 bg-opacity-85 shadow-md rounded-r-xl p-4 flex flex-col mr-4 mt-56 w-80"
-            }
-          >
-            <h3
-              className={
-                playlist
-                  ? "text-lg font-semibold pb-2 text-left leading-4 break-all"
-                  : "text-2xl font-bold pb-2 text-left"
-              }
-            >
-              {playlist ? playlist.title : title}
+          <img src={playlist.thumbnail} alt="thumbnail" />
+          <div className="absolute z-20 bg-purple-800 bg-opacity-85 shadow-md rounded-r-xl px-2 py-2 flex flex-col w-5/6 h-2/6 mt-16">
+            <h3 className="text-lg font-semibold pb-2 text-left leading-4 break-all">
+              {playlist.title}
             </h3>
           </div>
         </button>
       </div>
-      {playlist && (
-        <p className="text-white-500 text-sm text-left leading-4 mt-2 pb-1">
-          {playlist.description.length > 75
-            ? playlist.description.slice(0, 75).concat("...")
-            : playlist.description}
-        </p>
-      )}
+
+      <p className="text-white-500 text-sm text-left leading-4 mt-2 pb-1">
+        {playlist.description.length > 75
+          ? playlist.description.slice(0, 75).concat("...")
+          : playlist.description}
+      </p>
     </div>
   );
 }
 
 PlayListCard.propTypes = {
-  title: string,
   location: string.isRequired,
-  playlist: object,
+  playlist: object.isRequired,
   slide: bool,
 };
 
 PlayListCard.defaultProps = {
-  title: "",
-  playlist: null,
   slide: false,
 };
 
