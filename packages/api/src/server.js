271ee68936529e434c8191ac7f85c7c58bb1b485
Fabrizio Ervini <faber.rvn@gmail.com>
2021-05-26 15:17:31 +0200

    feat: add start script

--- packages/api/src/server.js
+++ packages/api/src/server.js
@@ -12,6 +12,7 @@ const {
   playlistRouter,
   genreRouter,
 } = require("./routes");
+const { config } = require("./config");
 
 const app = express();
 
@@ -21,7 +22,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: "true" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
 app.use(
   cors({
-    origin: "*",
+    origin: config.client.url,
     methods: ["GET", "PUT", "POST", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     optionsSuccessStatus: 200,
